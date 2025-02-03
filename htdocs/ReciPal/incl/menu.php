282e4f1e0afb60fdf41fc6644bc3ff4779e72d94
ArtiamL <main.lnewell@gmail.com>
2025-02-03 15:48:50 +0000

    Merged index.php, menu.php, and styles.css from zahra's implementation.

--- htdocs/ReciPal/incl/menu.php
+++ htdocs/ReciPal/incl/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">ReciPal</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -23,34 +23,28 @@
                 <li class="nav-item">
                     <a class="nav-link" href="./categories.php">About</a>
                 </li>
-                <?php if (!isset($_SESSION['username'])): ?>
-                    <li class="nav-item">
-                        <button type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#loginModal">
-                            <i class="fa-solid fa-user"></i> Login/Sign Up
-                        </button>
-                    </li>
-                <?php else: ?>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa-solid fa-user"></i> <?php echo $_SESSION['username'] ?>
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#"><i class="fa-solid fa-user"></i> Profile</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear"></i> Settings</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li>
-                            <form action="./api/logout" method="POST">
-                                <button type="submit" class="dropdown-item"><i class="fa-solid fa-power-off"></i> Logout</button>
-                            </form>
-                        </li>
-                      </ul>
-                    </li>
-                <?php endif; ?>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <?php if (!isset($_SESSION['username'])): ?>
+                <button type="button" class="btn btn-light me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+                    <i class="fa-solid fa-user"></i> Login/Sign Up
+                </button>
+            <?php else: ?>
+                <div class="dropdown">
+                  <button class="btn btn-light me-2 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-user"></i> <?php echo $_SESSION['username'] ?>
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#"><i class="fa-solid fa-user"></i> Profile</a></li>
+                    <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear"></i> Settings</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <form action="./api/logout" method="POST">
+                            <button type="submit" class="dropdown-item"><i class="fa-solid fa-power-off"></i> Logout</button>
+                        </form>
+                    </li>
+                  </ul>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </nav>
--- htdocs/ReciPal/index.php
+++ htdocs/ReciPal/index.php
@@ -15,6 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <link href="./styles/styles.css" rel="stylesheet">
 
     <script src="https://kit.fontawesome.com/7f67cc5608.js" crossorigin="anonymous" defer></script>
 
@@ -28,17 +29,76 @@
 </header>
 
 <main>
-    <h1>ReciPal</h1>
-    <section id="curated_posts">
-        <h2>We Love these Recipes!</h2>
-        <?php //loop through curated posts ?>
-        <div class="recipe_posts"></div>
-    </section>
-    <section id="newest_posts">
-        <h2>Newest Recipes</h2>
-        <!--            <hr>-->
-        <div class="recipe_posts"></div>
-    </section>
+    <div id="foodCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/recipebanner1jpg.jpg" class="d-block w-100" alt="Biryani">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/recipebanner2webp.webp" class="d-block w-100" alt="Chickpea Curry">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/recipe3.webp" class="d-block w-100" alt="Chicken Tikka Masala">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/recipe4.avif" class="d-block w-100" alt="Chicken Pie">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#foodCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#foodCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+    </div>
+    <h1 class="text-center mt-4">Our Top Trending Recipes</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="./img/biryani.jpg" class="card-img-top" alt="Biryani">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <strong>Biryani</strong> – A fragrant and flavorful rice dish made with marinated meat, aromatic spices, and basmati rice, slow-cooked to perfection.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="./img/chichpeacurry.jpg" class="card-img-top" alt="Chickpea Curry">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <strong>Chickpea Curry (Chana Masala)</strong> – A hearty dish featuring chickpeas in a rich tomato-based gravy with warm spices. Best with rice or naan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="./img/chicken-tikka-masala-73901-2.jpeg" class="card-img-top" alt="Chicken Tikka Masala">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <strong>Chicken Tikka Masala</strong> – Succulent grilled chicken chunks in a creamy tomato and butter sauce, packed with bold Indian flavors.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="./img/chickenpie.avif" class="card-img-top" alt="Chicken Pie">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <strong>Chicken Pie</strong> – A classic comfort food with a flaky, buttery crust filled with tender chicken, vegetables, and a creamy sauce.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 
